7841de38b7530a0e07a478afa06d7ecdad9fb506
Juyeong Jeon <human109@gmail.com>
2022-10-27 14:16:39 +0900

    [level1] Running Sum of 1D Array

---
+++ algorithms/leetcode/leetcode75/running_sum_of_1d_array.js
@@ -0,0 +1,21 @@
+/*
+* Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+* Return the running sum of nums.
+
+* Input: nums = [1,2,3,4]
+* Output: [1,3,6,10]
+* Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var runningSum = function(nums) {
+    nums.reduce((acc, cur, i, arr) => {
+        return arr[i] += acc;    
+    });
+    
+    return nums;
+};
