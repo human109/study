bdef5748c1ef30954176a4a9a7e1bd1c4d737d56
Juyeong Jeon <human109@gmail.com>
2022-11-10 01:20:00 +0900

    [Hello] Refactoring Test code

--- golang/hello_test.go
+++ golang/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Joo")
-	want := "Hello, Joo"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Joo")
+		want := "Hello, Joo"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
