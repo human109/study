dae13d2a4e55a3298030880b855fe11417aef9d0
Juyeong Jeon <human109@gmail.com>
2022-11-14 15:55:15 +0900

    [Iteration] example2 - refactor iteration

--- golang/day3/repeat.go
+++ golang/day3/repeat.go
@@ -1,9 +1,8 @@
 package iteration
 
+import "strings"
+
 func Repeat(character string, nums int) string {
-	var repeated string
-	for i := 0; i < nums; i++ {
-		repeated = repeated + character
-	}
+	repeated := strings.Repeat(character, nums)
 	return repeated
 }
