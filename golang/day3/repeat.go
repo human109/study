a08f99953646a40abc527181a677fc1a5fab0571
Juyeong Jeon <human109@gmail.com>
2022-11-14 15:49:44 +0900

    [Iteration] example - count of repeat

--- golang/day3/repeat.go
+++ golang/day3/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(character string) string {
+func Repeat(character string, nums int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < nums; i++ {
 		repeated = repeated + character
 	}
 	return repeated
